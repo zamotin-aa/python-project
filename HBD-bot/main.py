93ea26f1d6c77987800c3f49f740ec51469d58b3
zamotin-aa <zamotin.info@gmail.com>
2023-07-31 19:51:55 +0300

    2 HBD

--- HBD-bot/main.py
+++ HBD-bot/main.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_excel("C:\\Users\\PC\\Desktop\\NEW2_PY\\IFT_emp.xlsx", sheet_name='TDSheet')
+df = pd.read_excel("C:\\Users\\PC\\Desktop\\Учебное2023\\python-project\\HBD-bot\\IFT_emp.xlsx", sheet_name='TDSheet')
 
 #поменял значения на datetime
 df['Дата рождения'] = pd.to_datetime(df['Дата рождения'], dayfirst=True)
 df['Дата увольнения'] = pd.to_datetime(df['Испытательный срок'], dayfirst=True)
 
-#удалил ненужные столбцы
-df.drop(['Подразделение', 'Организация', 'Табельный номер','Должность', 'Испытательный срок' ], axis= 1 , inplace= True )
+#оставил только тех, кто еще работает (дата увольнения пустая)      
+df = df[df['Дата увольнения'].isnull()]
 
+#удалил ненужные столбцы
+df.drop(['Подразделение', 'Организация', 'Табельный номер', 
+         'Дата увольнения', 'Испытательный срок', 'Должность',], axis= 1 , inplace= True )
 
 print(df)
+
+

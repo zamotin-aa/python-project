7a4fe038637178687ff662cd1f347ff853aeaefc
zamotin-aa <zamotin.info@gmail.com>
2023-07-31 20:26:31 +0300

    3 HBD

--- HBD-bot/main.py
+++ HBD-bot/main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime, timedelta
+
+today = datetime.now().date()
+next_week = today + timedelta(days=7)
 
 df = pd.read_excel("C:\\Users\\PC\\Desktop\\Учебное2023\\python-project\\HBD-bot\\IFT_emp.xlsx", sheet_name='TDSheet')
 
 #поменял значения на datetime
 df['Дата рождения'] = pd.to_datetime(df['Дата рождения'], dayfirst=True)
+df['Дата приема'] = pd.to_datetime(df['Дата приема'], dayfirst=True)
 df['Дата увольнения'] = pd.to_datetime(df['Испытательный срок'], dayfirst=True)
 
 #оставил только тех, кто еще работает (дата увольнения пустая)      
@@ -14,6 +19,12 @@ df = df[df['Дата увольнения'].isnull()]
 df.drop(['Подразделение', 'Организация', 'Табельный номер', 
          'Дата увольнения', 'Испытательный срок', 'Должность',], axis= 1 , inplace= True )
 
-print(df)
 
+#список сотрудников, у которых ДР сегодня или в следующую неделю
+
+filtered_data = df[(df['Дата рождения'].dt.strftime('%m-%d') >= today.strftime('%m-%d')) &
+                   (df['Дата рождения'].dt.strftime('%m-%d') <= next_week.strftime('%m-%d'))]
+
+
+print(filtered_data)
 
